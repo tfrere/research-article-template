d61f1567825746178f61adb4d2c82df1f0987bbc
thibaud frere <thibaudfrere@mbp-de-thibaud.home>
2025-09-01 11:44:47 +0200

    update

--- app/src/env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../.astro/types.d.ts" />
-/// <reference types="vite/client" />
-
-declare module '*.png?url' {
-  const src: string;
-  export default src;
-}
-
-// (Global window typings for Plotly/D3 are intentionally omitted; components handle typing inline.)
